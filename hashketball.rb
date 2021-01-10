3ef6730020a563a53749896bd9f4d686d7f442b8
Aaron Muesing <muesing.music@gmail.com>
2021-01-10 15:53:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -184,7 +184,6 @@ def player_stats(player_name)
   player_stats = {}
   game_hash.each do |team, team_details_hash|
     team_details_hash[:players].each do |stats|
-
       if stats[:name] == player_name
         stats.delete(:name)
         player_stats = stats
