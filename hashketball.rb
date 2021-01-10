a5e9df199b20665c28ee639b92dc728ff5b2885b
Aaron Muesing <muesing.music@gmail.com>
2021-01-10 16:06:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -195,15 +195,15 @@ def player_stats(input)
 end
 
 def big_shoe_rebounds
-  shoes = 0
+  big_shoe = 0
   rebounds = 0
-  game_hash.each do |key, value|
-    value[:players].each do |variable|
-      if variable[:shoe] > shoes
-        shoes = variable[:shoe]
-        rebounds = variable[:rebounds]
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player|
+      if player[:shoe] > big_shoe
+        big_shoe = player[:shoe]
+        rebounds = player[:rebounds]
       end
     end
   end
-  return variable
+  return rebounds
 end
